5f7378e8586b4878b9c7919807878f1319f4644f
amelia15 <rina.sl@ukr.net>
2019-12-05 18:14:33 +0200

    Create entry.php

---
+++ entry.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    
+    public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // данные в $model удачно проверены
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // либо страница отображается первый раз, либо есть ошибка в данных
+            return "$this->render('entry', ['model' => $model])";
+        }
+    }
+}
